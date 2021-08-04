5853cb47be56e2734264b1b31f7d7bb73975d098
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-08-04 17:06:15 +0900

    Modify server port Number

--- functions/movie.js
+++ functions/movie.js
@@ -1,26 +1,13 @@
-const axios = require('axios')
-const { OMDB_API_KEY } = process.env
+import { url } from "../../utils/axios";
 
-exports.handler = async function(event){
-    console.log(event)
-    const payload = JSON.parse(event.body)
-    const { title, type, year, page, id } = payload
-    const url = id
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
+exports.handler = async function(){
     try {
-        const { data } = await axios.get(url)
-        if(data.Error){
-            return {
-                statusCode: 400,
-                body: data.Error
-            }
-        }
-        return{
-            statusCode: 200,
-            body: JSON.stringify(data)
-        }
+        const response = await fetch(url);
+        const data = await response.json();
+        return { 
+            statusCode: 200, 
+            body: JSON.stringify({ data }) 
+        };
     }catch(error){
         return {
             statusCode : error.response.status,
